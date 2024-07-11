4c257158d945318422fa804add1602f355038f92
Ferivonus <ferivonus@hotmail.com>
2024-07-11 23:26:36 +0300

    first rust discord bot commit

---
+++ src/main.rs
@@ -0,0 +1,47 @@
+use dotenv::dotenv;
+use serenity::async_trait;
+use serenity::model::channel::Message;
+use serenity::model::gateway::Ready;
+use serenity::prelude::*;
+use sqlx::MySqlPool;
+use std::env;
+use std::error::Error;
+
+struct Bot;
+
+#[async_trait]
+impl EventHandler for Bot {
+    async fn message(&self, ctx: Context, msg: Message) {
+        if msg.content == "!hello" {
+            if let Err(e) = msg.channel_id.say(&ctx.http, "world!").await {
+                println!("Error sending message: {:?}", e);
+            }
+        }
+    }
+
+    async fn ready(&self, _: Context, ready: Ready) {
+        println!("{} is connected!", ready.user.name);
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    dotenv().ok();
+    let token = env::var("DISCORD_TOKEN")?;
+
+    // Get the database URL from the environment variables and create a MySQL pool
+    // let database_url = env::var("DATABASE_URL")?;
+    // let _pool = MySqlPool::connect(&database_url).await?;
+
+    // Set gateway intents, which decides what events the bot will be notified about
+    let intents = GatewayIntents::GUILD_MESSAGES | GatewayIntents::MESSAGE_CONTENT;
+
+    let mut client = Client::builder(&token, intents)
+        .event_handler(Bot)
+        .await
+        .expect("Error creating client");
+
+    client.start().await?;
+
+    Ok(())
+}
